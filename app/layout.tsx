fa4fd15ff3f443de449cee71073414ea287a6340
Daniel Atoche-Juarez <dtoch123@gmail.com>
2024-09-03 17:28:01 -0400

    Got rid of warnings in the console.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <HeroBanner /> {/* Include the HeroBanner component */}
+        <HeroBanner />
         <main>{children}</main>
         <Footer />
       </body>
