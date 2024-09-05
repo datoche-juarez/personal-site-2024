7071256d7cb99d1a62ede395e92147ecd757314a
Daniel Atoche-Juarez <dtoch123@gmail.com>
2024-09-04 20:48:09 -0400

    Contact Form is now working properly with the API. Contact Me section is complete.

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,7 +1,82 @@
 "use client";
+import { useState } from "react";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+interface StatusState {
+  success: boolean | null;
+  message: string;
+}
+
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const [status, setStatus] = useState<StatusState>({
+    success: null,
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setStatus({ success: null, message: "" });
+
+    const combinedData = {
+      Name: `${formData.firstName} ${formData.lastName}`,
+      Email: formData.email,
+      Mobile: formData.phone,
+      Message: formData.message,
+      Service: "danielatochejuarez.com",
+    };
+
+    try {
+      const response = await fetch(`${API_URL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(combinedData),
+      });
+
+      if (response.ok) {
+        setStatus({ success: true, message: "Message sent successfully!" });
+        setFormData({
+          firstName: "",
+          lastName: "",
+          email: "",
+          phone: "",
+          message: "",
+        });
+      } else {
+        setStatus({
+          success: false,
+          message: "Failed to send the message. Please try again later.",
+        });
+      }
+    } catch (error) {
+      setStatus({
+        success: false,
+        message: "There was an error. Please try again.",
+      });
+    }
+  };
+
   return (
-    <form className="mt-8 md:mt-10">
+    <form className="mt-8 md:mt-10" onSubmit={handleSubmit}>
       <div className="mb-4 flex flex-col md:flex-row gap-4">
         <div className="w-full md:w-1/2">
           <label className="block text-black mb-2 font-medium">
@@ -9,14 +84,22 @@ const ContactForm = () => {
           </label>
           <input
             type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
             className="w-full h-10 border border-[#767676] rounded"
+            required
           />
         </div>
         <div className="w-full md:w-1/2">
           <label className="block text-black mb-2 font-medium">Last Name</label>
           <input
             type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
             className="w-full h-10 border border-[#767676] rounded"
+            required
           />
         </div>
       </div>
@@ -24,28 +107,53 @@ const ContactForm = () => {
         <div className="w-full md:w-1/2">
           <label className="block text-black mb-2 font-medium">Email</label>
           <input
-            type="text"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
             className="w-full h-10 border border-[#767676] rounded"
+            required
           />
         </div>
         <div className="w-full md:w-1/2">
           <label className="block text-black mb-2 font-medium">Phone</label>
           <input
-            type="text"
+            type="tel"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
             className="w-full h-10 border border-[#767676] rounded"
+            required
           />
         </div>
       </div>
       <div className="mb-4">
         <label className="block text-black mb-2 font-medium">Message</label>
-        <textarea className="w-full h-52 border border-[#767676] rounded"></textarea>
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          className="w-full h-52 border border-[#767676] rounded"
+          required
+        ></textarea>
       </div>
       <button
         type="submit"
-        className="w-full xs:w-[200px] hover:bg-teal  py-2 bg-black text-white text-base rounded"
+        className="w-full xs:w-[200px] hover:bg-teal py-2 bg-black text-white text-base rounded"
       >
         Submit
       </button>
+
+      {/* Show success or error message */}
+      {status.success !== null && (
+        <div
+          className={`mt-4 p-2 rounded ${
+            status.success ? "text-gray-500" : " text-red-800"
+          }`}
+        >
+          {status.message}
+        </div>
+      )}
     </form>
   );
 };
