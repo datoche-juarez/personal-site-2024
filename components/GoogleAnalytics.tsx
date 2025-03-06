4a9de23d55d1a786fdd73ebc7b20792760c12d6d
Daniel Atoche-Juarez <dtoch123@gmail.com>
2025-03-05 23:47:37 -0500

    Removed gtag log.

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -4,7 +4,6 @@ import Script from "next/script";
 
 // Log the tracking ID to verify it's being injected at build time
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "NOT_DEFINED";
-console.log("🚀 GA_TRACKING_ID inside component:", GA_TRACKING_ID);
 
 export default function GoogleAnalytics() {
   if (!GA_TRACKING_ID || GA_TRACKING_ID === "NOT_DEFINED") {
